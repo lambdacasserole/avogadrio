74865eccb81a8d25915aff196d6adafc8dbe49e4
Saul Johnson <saul.a.johnson@gmail.com>
2017-07-30 15:39:46 +0100

    Major UI improvement

--- web/js/main.js
+++ web/js/main.js
@@ -1,23 +1,44 @@
 $(document).ready(function () {
-    var fgc = "000000";
-    var bgc = "ffffff";
+    var foregroundColor = "ffffff";
+    var backgroundColor = "000000";
         
-    function ref() {
-        $('body').attr('style', 'background:url(/api/name/1920/1080/' + fgc + '/caffeine);background-position:50% 50%;background-color:#' + bgc + ";background-repeat:no-repeat;");
+    function buildUrl(width, height, foreground, background, name) {
+        return '/api/name/' + width + '/' + height + '/' + foreground + '/' + background + '/' + name;
+    }
+    
+    function buildMoleculeOnlyUrl(width, height, foreground, name) {
+        return '/api/name/' + width + '/' + height + '/' + foreground + '/' + name;
+    }
+        
+    function refreshPreview() {
+        $('body')
+            .css('background', 'url(' + buildMoleculeOnlyUrl(1920, 1280, foregroundColor, $('.comp-name').val()) + ')')
+            .css('background-color', '#' + bgc)
+            .css('background-position', '50% 50%')
+            .css('background-repeat', 'no-repeat');
     }
         
     $('.picker-fg').on('change', function(e) {
         fgc = $(e.target).val().substring(1);
-        ref();
+        refreshPreview();
     });
+    
     $('.picker-bg').on('change', function(e) {
         bgc = $(e.target).val().substring(1);
-        ref();
+        refreshPreview();
+    });
+    
+    $('.update-btn').on('click', function(e) {
+        refreshPreview();
+    });
+    
+    $('.download-btn').on('click', function(e) {
+        
     });
 
     $(".picker").spectrum({
         // color: tinycolor,
-        flat: true,
+        // flat: true,
         // showInput: true,
         // showInitial: true,
         // allowEmpty: bool,
@@ -40,6 +61,8 @@ $(document).ready(function () {
         // palette: [[string]],
         // selectionPalette: [string]
     });
+    
+    refreshPreview();
 });
 
         
